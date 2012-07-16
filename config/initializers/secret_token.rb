ef69ab79bbe86226c95907fe2ebef410d75815a9
Sterling Bates <sterling@stepresearch.com>
2012-07-16 16:05:31 -0400

    initial blank app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quiz::Application.config.secret_token = '2cc7e326d408847dbb14c94ae5b0ee0217d88e9b2aa4f2197ebeffb8f55497e7a5cf6a7fb25a1c4b7945c8a34eda6ef32fb5aea89a6dbae89696c399cd464213'
